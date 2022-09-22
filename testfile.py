5b4d5b2c98bedae59d1c6a16176b3b7cf992e6a9
Mominthaj <92154970+Mominthaj@users.noreply.github.com>
2022-09-22 17:12:05 +0530

    Add files via upload

---
+++ testfile.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return"Test code is working!!!"
+
+@app.route('/next')
+def next():
+    return"Showing the next page!!"
+
+
+if __name__ == '__main__':
+  app.run()
